1fd0aceffacac1eb5938dd7a8a07ac4a45fc9779
ophthalmologist <gusrylmubarok@gmail.com>
2022-04-01 13:32:20 +0700

    Change Button Sidebar Dashboard

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -13,44 +13,40 @@
 
 	  <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#layanan-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-menu-button-wide"></i><span>Data Layanan</span><i class="bi bi-chevron-down ms-auto"></i>
+          <i class="bi bi-menu-button-wide"></i><span>Kelola Master</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
         <ul id="layanan-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
             <a href="components-alerts.html">
-              <i class="bi bi-circle"></i><span>Harga Layanan</span>
+              <i class="bi bi-circle"></i><span>Data User</span>
             </a>
           </li>
           <li>
             <a href="components-accordion.html">
-              <i class="bi bi-circle"></i><span>Tipe Layanan</span>
-            </a>	
+              <i class="bi bi-circle"></i><span>Data Pelanggan</span>
+            </a>
+					</li>
+					<li>
+            <a href="components-accordion.html">
+              <i class="bi bi-circle"></i><span>Data Jasa Cuci</span>
+            </a>
+					</li>		
 		 </li>
         </ul>
-      </li><!-- End Layanan Nav -->
+      </li><!-- End Master Nav -->
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#pelanggan-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-menu-button-wide"></i><span>Data Pelanggan</span><i class="bi bi-chevron-down ms-auto"></i>
+			<li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#transaksi-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-layout-text-window-reverse"></i><span>Transaksi</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
-        <ul id="pelanggan-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+        <ul id="transaksi-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
-            <a href="components-alerts.html">
-              <i class="bi bi-circle"></i><span>Pelanggan Baru</span>
+            <a href="tables-general.html">
+              <i class="bi bi-circle"></i><span>Data Transaksi Pelanggan</span>
             </a>
           </li>
-          <li>
-            <a href="components-accordion.html">
-              <i class="bi bi-circle"></i><span>Repeat Order</span>
-            </a>	
-		 </li>
-		 <li>
-            <a href="components-accordion.html">
-              <i class="bi bi-circle"></i><span>Member Bulanan</span>
-            </a>	
-		 </li>
         </ul>
-      </li><!-- End Pelanggan Nav -->
+      </li><!-- End Transaksi Nav -->
 
       <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#karyawan-nav" data-bs-toggle="collapse" href="#">
@@ -81,77 +77,29 @@
       </li><!-- End Karyawan Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#transaksi-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-layout-text-window-reverse"></i><span>Data Transaksi</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="transaksi-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="tables-general.html">
-              <i class="bi bi-circle"></i><span>Transaksi Pengeluaran</span>
-            </a>
-          </li>
-          <li>
-            <a href="tables-data.html">
-              <i class="bi bi-circle"></i><span>Transaksi Pemasukan</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Transaksi Nav -->
-
-      <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#laporan-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-bar-chart"></i><span>Data Laporan</span><i class="bi bi-chevron-down ms-auto"></i>
+          <i class="bi bi-bar-chart"></i><span>Laporan</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
         <ul id="laporan-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
             <a href="charts-chartjs.html">
-              <i class="bi bi-circle"></i><span>Pengeluaran</span>
+              <i class="bi bi-circle"></i><span>Laporan Pelanggan</span>
             </a>
           </li>
           <li>
             <a href="charts-apexcharts.html">
-              <i class="bi bi-circle"></i><span>Pemasukan</span>
+              <i class="bi bi-circle"></i><span>Laporan Jasa Cuci</span>
             </a>
           </li>
-          </li>
-		  <li>
-            <a href="charts-chartjs.html">
-              <i class="bi bi-circle"></i><span>Stok Bahan Baku</span>
+					<li>
+            <a href="charts-apexcharts.html">
+              <i class="bi bi-circle"></i><span>Laporan Transaksi</span>
             </a>
           </li>
         </ul>
       </li><!-- End Laporan Nav -->
 
 	  <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#kurir-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-bar-chart"></i><span>Data Kurir</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="kurir-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="charts-chartjs.html">
-              <i class="bi bi-circle"></i><span>Data Pengantaran Barang</span>
-            </a>
-          </li>
-          <li>
-            <a href="charts-apexcharts.html">
-              <i class="bi bi-circle"></i><span>Data Penjemputan Barang</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Kurir Nav -->
-
-	  <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#pengguna-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-bar-chart"></i><span>Data Pengguna</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="pengguna-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="charts-chartjs.html">
-              <i class="bi bi-circle"></i><span>List Pengguna Aplikasi</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Kurir Nav -->
 
       <li class="nav-heading">Pages</li>
 
@@ -161,42 +109,6 @@
           <span>Profile</span>
         </a>
       </li><!-- End Profile Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-faq.html">
-          <i class="bi bi-question-circle"></i>
-          <span>F.A.Q</span>
-        </a>
-      </li><!-- End F.A.Q Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-contact.html">
-          <i class="bi bi-envelope"></i>
-          <span>Contact</span>
-        </a>
-      </li><!-- End Contact Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-login.html">
-          <i class="bi bi-box-arrow-in-right"></i>
-          <span>Login</span>
-        </a>
-      </li><!-- End Login Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-error-404.html">
-          <i class="bi bi-dash-circle"></i>
-          <span>Error 404</span>
-        </a>
-      </li><!-- End Error 404 Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-blank.html">
-          <i class="bi bi-file-earmark"></i>
-          <span>Blank</span>
-        </a>
-      </li><!-- End Blank Page Nav -->
-
     </ul>
 
   </aside><!-- End Sidebar-->
